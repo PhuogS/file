2deb583cb6c3858ae4167a5a058d21c8bff1bf35
PhuogS <86711046+PhuogS@users.noreply.github.com>
2021-07-01 09:24:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
     introTitle: 'Anh Chiến à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say anh ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
+    introDesc: `Em có điều này muốn nói với anh`,
+    btnIntro: 'Bấm vào đây anh nhé',
+    title: 'Anh có iuuu eemmm hoonggg ??????? 😙',
+    desc: 'THOÁT RA LÀM CHỒNG EM :)))))))))) ',
+    btnYes: 'Okkkkk iiuuuuu emmmm <33',
     btnNo: 'Không, cô bé à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question:'Lí do em thích em là gì ??',
+    btnReply: 'Gửi đến em <3',
+    reply: 'Vì em dethuonggg vllllllllllll <33333333',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón em nhé.',
     btnAccept: 'Okiiiii lun <3',
